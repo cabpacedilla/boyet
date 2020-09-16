fee7ffd906801b72279c174e96e88260b8e20d53
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2020-09-16 18:20:30 +0800

    Update laptopLid_close.sh

--- laptopLid_close.sh
+++ laptopLid_close.sh
@@ -5,17 +5,16 @@
 #!/usr/bin/bash
 while true
 do
-   lid_closed=`cat /proc/acpi/button/lid/LID0/state | grep -P -o 'close'`
-   lid_open=`cat /proc/acpi/button/lid/LID0/state | grep -P -o 'open'`
-
+   lid_closed=`less /proc/acpi/button/lid/LID0/state | grep -P -o 'close'`
+   lid_open=`less /proc/acpi/button/lid/LID0/state | grep -P -o 'open'`
+   
    if [ "$lid_closed" = close ]
    then
       xscreensaver-command -lock
-
+      
    elif [ "$lid_open" = open ]
    then
       :
-
+      
    fi
 done
-
