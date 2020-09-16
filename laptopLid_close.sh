2ea20948401af9147f1dcb9915eaf6fadb4e237f
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2020-09-16 13:00:17 +0800

    Update laptopLid_close.sh

--- laptopLid_close.sh
+++ laptopLid_close.sh
@@ -1,5 +1,7 @@
-#!/usr/bin/bash
+# This script will activate the xscreensaver lock when the laptop lid will be closed for auto lid close security in Ubuntu
+# September 2020
 
+#!/usr/bin/bash
 while true
 do
    lid_closed=`cat /proc/acpi/button/lid/LID0/state | grep -P -o 'close'`
@@ -7,7 +9,7 @@ do
 
    if [ "$lid_closed" = close ]
    then
-           xscreensaver-command -lock  
+      xscreensaver-command -lock
 
    elif [ "$lid_open" = open ]
    then
