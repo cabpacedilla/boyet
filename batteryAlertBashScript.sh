c028ec1e19af56fca5a5e39727ac05170947da0c
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2020-09-29 13:49:44 +0800

    Update batteryAlertBashScript.sh

--- batteryAlertBashScript.sh
+++ batteryAlertBashScript.sh
@@ -7,12 +7,13 @@
 # I added discharging and charging variables and conditions to detect the discharging and charging state of the battery.
 
 # 1. Install acpi
-# 2. Create a bin directory inside your home directory
-# 3. Change directory to the bin directory
-# 4. Create the bash script file below with nano or gedit and save it with a filename like batAlert.sh
-# 5. Make file executable with chmod +x battAlert.sh
-# 6. Add the battAlert.sh command in Startup applications
-# 7. Reboot the laptop
+# 2. Install vlc
+# 3. Create a bin directory inside your home directory
+# 4. Change directory to the bin directory
+# 5. Create the bash script file below with nano or gedit and save it with a filename like batAlert.sh
+# 6. Make file executable with chmod +x battAlert.sh
+# 7. Add the battAlert.sh command in Startup applications
+# 8. Reboot the laptop
 
 #!/usr/bin/bash
 while true
@@ -20,11 +21,11 @@ do
    battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
    battery_charge=`acpi -b | grep -P -o 'Charging'`
    battery_discharge=`acpi -b | grep -P -o 'Discharging'`
-   
+
    if [ $battery_level -le 40 ] && [ "$battery_discharge" = Discharging ]
    then
       notify-send "Battery reached ${battery_level}%, plug the power cable to optimize battery life!"
-      gnome-terminal -x nvlc ~/Music/low_batt.mp3
+      gnome-terminal -- nvlc --play-and-exit ~/Music/low_battery.mp3 
       
    elif [ $battery_level -le 40 ] && [ "$battery_charge" = Charging ]
    then
@@ -33,8 +34,9 @@ do
    elif [ $battery_level -ge 80 ] && [ "$battery_charge" = Charging ]
    then
       notify-send "Battery reached ${battery_level}%, unplug the power cable to optimize battery life!" 
-      gnome-terminal -x nvlc ~/Music/full_batt.mp3
-         
+      gnome-terminal -- nvlc --play-and-exit ~/Music/glados_bat_full_2.mp3 
+      
    fi
+   
    sleep 60
 done
