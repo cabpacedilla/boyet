8578749cec8ed09b1708cea8c5bcef436551735d
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2020-10-03 07:15:20 +0800

    Update laptopLid_close.sh

--- laptopLid_close.sh
+++ laptopLid_close.sh
@@ -2,13 +2,17 @@
 # This script was written by Claive Alvin P. Acedilla. It can be copied, modified and redistributed.
 # September 2020
 
+# Steps:
 # 1. Install ligthdm display manager and set lightdm as a the default display manager
-# 2. Create the bash script file below with nano or gedit and save it with a filename like lid_closed.sh in ~/bin directory
-# 3. Make file executable with chmod +x lid_closed.sh command
-# 4. Add the lid_closed.sh command in Startup applications
-# 5. Reboot the laptop
-# 6. Close the laptop lid
-# 7. lightdm login screen will 
+# 2. Create a bin directory inside your home directory
+# 3. Change directory to the bin directory
+# 4. Create the bash script file below with nano or gedit and save it with a filename like lid_closed.sh
+# 5. Make file executable with chmod +x lid_closed.sh command
+# 6. Add the lid_closed.sh command in Startup applications
+# 7. Reboot the laptop
+# 8. Close the laptop lid
+# 9. Open the laptop lid
+# 10. lightdm login screen will be displayed
 
 #!/usr/bin/bash
 while true
