21cc6dee697a647a25a3a754ce25f7dd33570bad
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2020-10-03 07:07:42 +0800

    Update laptopLid_close.sh

--- laptopLid_close.sh
+++ laptopLid_close.sh
@@ -1,4 +1,4 @@
-# This script will activate the xscreensaver lock when the laptop lid will be closed for auto lid close security in Ubuntu
+# This script will activate screen lock when the laptop lid will be closed for auto lid close security in Ubuntu
 # This script was written by Claive Alvin P. Acedilla. It can be copied, modified and redistributed.
 # September 2020
 
