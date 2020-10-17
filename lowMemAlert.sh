1f216704d64d37dc1a95a76c505949a2db866163
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2020-10-18 07:02:24 +0800

    Update lowMemAlert.sh

--- lowMemAlert.sh
+++ lowMemAlert.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/bash
 while true
 do
-   ## get total free memory size in megabytes(MB) 
-   free=$(free -mt | grep Total | awk '{print $4}')
+   free_mem=$(free_mem -mt | grep Total | awk '{print $4}')
 
-   ## check if free memory is less or equals to 20%
-   if [ $free -le 800 ] 
+   if [ $free_mem -le 800 ] 
    then
-        
-      ## get top processes consuming system memory and
       top_processes=`ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%mem | head` 
+      
       notify-send "RAM has low free memory. Free high memory consuming applications from the top memory consuming processes: ${top_processes}" 
   
    elif [ $free -gt 800 ]
