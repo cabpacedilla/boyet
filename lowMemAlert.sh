6f79faa9a163d418764a984e02d3ecbc81790667
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2020-10-18 07:10:19 +0800

    Update lowMemAlert.sh

--- lowMemAlert.sh
+++ lowMemAlert.sh
@@ -1,3 +1,17 @@
+# This script will alert when free memory is 20% or less
+# This script was written by Claive Alvin P. Acedilla. It can be copied, modified and redistributed.
+# October 2020
+
+# Steps for the task:
+# 1. Create a bin directory inside your home directory
+# 2. Change directory to the bin directory
+# 3. Create the bash script file below with nano or gedit and save it with a filename like lowMemAlert.sh
+# 4. Make file executable with chmod +x lowMemAlert.sh command
+# 5. Add the lowMemAlert.sh command in Startup applications
+# 6. Reboot the laptop
+# 7. Log in and simulate low memory scenario by running high memory consuming processes until free memory reaches 20% or less
+# 8. Low memory alert message will be displayed
+
 #!/usr/bin/bash
 while true
 do
@@ -16,4 +30,4 @@ do
    fi
 
    sleep 30
-done
+don
