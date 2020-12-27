6d1d20baf331ee7679cd3677885a63d468e9c722
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2020-12-27 20:39:59 +0800

    Update powerAlert.sh

--- powerAlert.sh
+++ powerAlert.sh
@@ -1,3 +1,19 @@
+# This script will alert when battery level is below or equal 40% and will notify when battery level is above or equal 80% to optimize laptop battery life.
+# This script was assembled and written by Claive Alvin P. Acedilla. It can be copied, modified and redistributed.
+# September 2020
+
+# Steps for the task:
+# 1. Install acpi
+# 2. Install vlc
+# 3. Create a bin directory inside your home directory
+# 4. Change directory to the bin directory
+# 5. Create the bash script file below with nano or gedit and save it with a filename like battAlert.sh
+# 6. Make file executable with chmod +x battAlert.sh command
+# 7. Add the battAlert.sh command in Startup applications
+# 8. Reboot and use the laptop until the battery drains to 40%
+# 9. A notification message will be displayed to plug the power cable to opitimize the battery life. Then a low battery notification sound will be played.
+# 10. Let the laptop charge until 80%
+# 11. A notification message will be displayed to unplug the power cable to opitimize the battery life. Then a full battery notification sound will be played.
 
 #!/usr/bin/bash
 notify()
