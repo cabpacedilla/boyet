2a7657120493da09ff1097a8a91265efb4bc5c46
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2020-12-28 09:14:06 +0800

    Update batteryAlertBashScript.sh

--- batteryAlertBashScript.sh
+++ batteryAlertBashScript.sh
@@ -28,7 +28,7 @@ notify()
    fi
     
    # notify to plug or unplug based on battery level
-   notify-send -t 1500 "Battery reached $2%. $ACTION the power cable to optimize battery life!"
+   notify-send -t 1500 "Battery reached ${2}%. ${ACTION} the power cable to optimize battery life!"
    
    # check if cvlc file program is existing then play low or full mp3
    if [ -f "$(which cvlc)" ]; then
@@ -44,19 +44,24 @@ do
    battery_discharge=$(acpi -b | grep -P -o 'Discharging')
    battery_full=$(acpi -b | grep -P -o 'Not charging')
 
-   if [ "$battery_level" -le 40 ] && [ "$battery_discharge" = 'Discharging' ]; then
-      # call notify function and pass low argument and battery level
-      notify low "$battery_level"
+   if [ "$battery_level" -le 40 ] && [ "$battery_discharge" = Discharging ]; then
+      # call notify function and pass low argument and battery level 
+      # if battery level is 40 or less and discharging
+      notify low ${battery_level}
       
    elif [ "$battery_level" -le 40 ] && [ "$battery_charge" = 'Charging' ]; then
+      # do nothing if battery level is 40 or less and charging
       :
     
    elif [ "$battery_level" -ge 80 ] &&  [ "$battery_charge" = 'Charging' ]; then
       # call notify function and pass full argument and battery level
-      notify full "$battery_level"
+      # if battery level is 80 or more and charging
+      notify full ${battery_level}
    
    elif [ "$battery_level" -ge 80 ] && [ "$battery_full" = 'Not charging' ]; then
-     notify full "$battery_level"
+      # call notify function and pass full argument and battery level
+      # if battery level is 80 or more and not charging
+     notify full ${battery_level}
       
    fi
    
