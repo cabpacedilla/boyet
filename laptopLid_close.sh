d895f1bb1a8cb9f4a4bc8961be7ea6cbffc80d07
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2021-01-03 10:49:20 +0800

    Update laptopLid_close.sh

--- laptopLid_close.sh
+++ laptopLid_close.sh
@@ -1,25 +1,27 @@
-# This script will activate screen lock when the laptop lid will be closed for auto lid close security in Ubuntu Unity
+# This script will activate screen lock when the laptop lid will be closed for auto lid close security in Linux
 # This script was written by Claive Alvin P. Acedilla. It can be copied, modified and redistributed.
 # September 2020
 
 # Steps for the task:
-# 1. Install the ligthdm display manager and set lightdm as a the default display manager
-# 2. Create a bin directory inside your home directory
-# 3. Change directory to the bin directory
-# 4. Create the bash script file below with nano or gedit and save it with a filename like lid_closed.sh
-# 5. Make file executable with chmod +x lid_closed.sh command
-# 6. Add the lid_closed.sh command in Startup applications
-# 7. Reboot the laptop
-# 8. Close the laptop lid
-# 9. Open the laptop lid
-# 10. lightdm login screen will be displayed
+# 1. Install icewm window manager 
+# 2. Install xscreensaver
+# 3. Create a bin directory inside your home directory
+# 4. Change directory to the bin directory
+# 5. Create the bash script file below with nano or gedit and save it with a filename like lid_close.sh
+# 6. Make file executable with chmod +x lid_close.sh command
+# 7. Add the lid_close.sh command in Startup
+# 8. Reboot the laptop
+# 9. Login to icewm
+# 10. Close the laptop lid
+# 11. Open the laptop lid
+# 12. xscreensaver will ask for password
 
 #!/usr/bin/bash
 while true
 do
-   lid_status=$(less /proc/acpi/button/lid/LID0/state | awk '{print $2}')
+   LID_STATE=$(less /proc/acpi/button/lid/LID0/state | awk '{print $2}')
    
-   if [ "$lid_status" = 'open' ]; then
+   if [ "$LID_STATE" = 'open' ]; then
       :
         
    else
