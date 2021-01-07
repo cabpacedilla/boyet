653d4560e80297ee830edfc253bcb3201a9120ff
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2021-01-07 11:29:45 +0800

    Update checkService.sh

--- checkService.sh
+++ checkService.sh
@@ -3,13 +3,12 @@
 declare -a SERVICES=("blueman-applet" "nm-applet")
 
 ctr=0   
-while [ "${SERVICES[$ctr]}" != "${SERVICES[-1]}" ] ; do
+while [ "$ctr" -le "${#SERVICES[@]}" ] ; do
    
    if pgrep -x "${SERVICES[$ctr]}" >/dev/null; then
       :
    
    else   
-      sleep 2 &&
       ${SERVICES[$ctr]} &
       
    fi
