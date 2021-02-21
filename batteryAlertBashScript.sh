a37f45bea8870cae052635aea6cf64d94c5d52dc
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2021-02-21 16:41:20 +0800

    Update batteryAlertBashScript.sh

--- batteryAlertBashScript.sh
+++ batteryAlertBashScript.sh
@@ -28,7 +28,7 @@ notify()
    fi
     
    # notify to plug or unplug based on battery level
-   notify-send -t 10000 "Battery reached ${2}%. ${ACTION} the power cable to optimize battery life!"
+   notify-send "Battery reached ${2}%. ${ACTION} the power cable to optimize battery life!"
    
    # check if cvlc file program is existing then play low or full mp3
    if [ -f "$(which cvlc)" ]; then
