bf79b50b5d88e27703ed1a71a39e74a60a466263
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2021-09-16 05:44:14 +0800

    Update autosync.sh

--- autosync.sh
+++ autosync.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-#install first kqueue in BSD systems or inotify-tools in Linux
+# Needs kqueue in BSD systems or inotify-tools in Linux
 
 while fswatch -1 ~/Documents/testfiles
 do    
