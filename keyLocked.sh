2f074742c8ebd9f00842e83fab2e58ed5397d2a0
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2022-02-27 04:57:40 +0800

    Update keyLocked.sh

--- keyLocked.sh
+++ keyLocked.sh
@@ -14,26 +14,33 @@
 # 9. Press the Num Lock key
 # 10. A Num Lock key notification message will be displayed
 
-#!/usr/bin/bash
+#!/usr/bin/sh
+
 while true
 do
-   # 1. Get LED mask value for key lock with xset command
+  # 1. Set key lock values
+	CAPS_LOCK=00000001
+	NUM_LOCK=00000002
+	CAPSNUM_LOCK=00000003
+	NO_LOCK=00000000
+	
+   # 2. Get LED mask value for key lock with xset command
    LED_MASK=$(xset q | grep 'LED mask' | awk '{ print $NF }')
  
-   # 2. If LED mask value is equal to Caps lock LED mask value, show Caps lock notification
-   if [ "$LED_MASK" = 00000001 ]; then
+   # 3. If LED mask value is equal to Caps lock LED mask value, show Caps lock notification
+   if [ "$LED_MASK" -eq "$CAPS_LOCK" ]; then
       notify-send "Caps lock is on."
    
-   # 2. If LED mask value is equal to Num lock LED mask value, show Num lock notification
-   elif [ "$LED_MASK" = 00000002 ]; then 
+   # 4. If LED mask value is equal to Num lock LED mask value, show Num lock notification
+   elif [ "$LED_MASK" -eq "$NUM_LOCK" ]; then 
       notify-send "Num lock is on."
        
-   # 3. If LED mask value is equal to Caps lock and Num lock LED mask value, show Caps lock and Num lock notification
-   elif  [ "$LED_MASK" = 00000003 ]; then
+   # 5. If LED mask value is equal to Caps lock and Num lock LED mask value, show Caps lock and Num lock notification
+   elif  [ "$LED_MASK" -eq "$CAPSNUM_LOCK" ]; then
       notify-send "Caps lock and Num lock are on."
       
-   # 4. If LED mask value is equal to LED mask value of no keys being locked, do nothing.   
-   elif  [ "$LED_MASK" = 00000000 ]; then
+   # 6. If LED mask value is equal to LED mask value of no keys being locked, do nothing.   
+   elif  [ "$LED_MASK" -eq "$NO_LOCK" ]; then
       :
    
    fi
