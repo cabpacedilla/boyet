66be30428e22d9a84daa47b15f92e002b712efd2
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2022-02-27 13:35:37 +0800

    Update autosync.sh

--- autosync.sh
+++ autosync.sh
@@ -1,11 +1,15 @@
-#!/usr/bin/bash
+#!/usr/bin/sh
+
 # fswatch needs filesystem change monitoring tool like inotify in Linux
 #DATE=`date | awk '{print $2$3$4$5$6}'`
 
-while fswatch -1 ~/Documents/testfiles
+#while fswatch -1 ~/Documents/testfiles
 
+while fswatch -1 ~/<sourcefolder>
+do
+   sudo rsync -avHAX ~/<sourcefolder>/ /<destinationfolder>/ --delete
 
-   sudo rsync -avHAX ~/Documents/testfiles/ /mnt/backup/ --delete
+   #sudo rsync -avHAX ~/Documents/testfiles/ /mnt/backup/ --delete
    #sudo tar cvf /mnt/backup/tarball$DATE ~/Documents/testfiles/*
 
 done
