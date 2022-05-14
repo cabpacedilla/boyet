7fb1e789c9db5f5be6df0c68f33f94f4fc215c26
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2022-05-15 02:33:31 +0800

    Update and rename autosync.sh to autobackupsync.sh

--- autobackupsync.sh
+++ autobackupsync.sh
@@ -1,12 +1,9 @@
+#!/usr/bin/sh
+
 # Set user in /etc/sudoers file without providing password with rsync command
 # add line <username> ALL=(ALL) NOPASSWD: /usr/bin/rsync
 
-#!/usr/bin/sh
-
-# fswatch needs filesystem change monitoring tool like inotify in Linux
-#DATE=`date | awk '{print $2$3$4$5$6}'`
-
-#while fswatch -1 ~/Documents/testfiles
+# Needs filesystem change monitoring tool like inotify in Linux
 
 while inotifywait modify ~/<sourcefolder>
 do
