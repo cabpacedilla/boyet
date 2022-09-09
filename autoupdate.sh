7103043fec5776f14f3094a86713105c18adcb94
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2022-09-09 11:41:55 +0800

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+# This script will automatically upgrade upgradeable packages.
+# This code was assembled and written by Claive Alvin P. Acedilla. It can be copied, modified and redistributed.
 
 UPGRADEABLE=$(sudo apt update | grep "packages can be upgraded")
 PACKAGENUM=$(echo $UPGRADEABLE | awk '{print $1}')
