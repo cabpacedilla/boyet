c6edb9fca2baeadf42b499c35f7aa0440a6ff311
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2022-09-09 13:05:11 +0800

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -12,10 +12,13 @@ UPGRADEABLE=$(sudo apt update | grep "packages can be upgraded")
 PACKAGENUM=$(echo $UPGRADEABLE | awk '{print $1}')
 
 if [ -n "$UPGRADEABLE" ]; then
-        notify-send "Upgrading $PACKAGENUM packages"
-        sudo apt upgrade
-
+	sudo apt list --upgradeable > ~/bin/upgradeable.txt
+	PACKAGES=$(cut -d/ -f1 upgradeable.txt)
+  	notify-send "Upgrading $PACKAGES"
+	sudo apt upgrade
+	
 else
-        notify-send "No upgradeable packages."
-
+	notify-send "No upgradeable packages."
+	
 fi
+	
