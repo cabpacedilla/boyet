375f741066a78ce4b27bfef0b84a6ec3280d1e04
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2022-10-17 15:51:28 +0800

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -11,17 +11,18 @@ LIST=~/bin/upgradeable.txt
 
 if [ "$DATE" = "01" ] || [ "$DATE" = "15" ]; then
 if [ -n "$UPGRADEABLE" ]; then
-	sudo apt list --upgradeable | tail -n +2 > $LIST
-	PACKAGES=$(cut -d/ -f1 $LIST)
-  	notify-send "Auto-updates:" "Upgrading $PACKAGES"	
-	if yes | sudo apt upgrade; then
-    	notify-send "Auto-updates:" "$PACKAGES were updated."
-    else
-    	notify-send "Auto-updates:" "Upgrade was unsuccessful."
-    fi 
+   sudo apt list --upgradeable | tail -n +2 > $LIST
+   PACKAGES=$(cut -d/ -f1 $LIST)
+   notify-send "Auto-updates:" "Upgrading $PACKAGES"	
+   if yes | sudo apt upgrade; then
+      notify-send "Auto-updates:" "$PACKAGES were updated."
+      yes | sudo apt autoremove
+   else
+      notify-send "Auto-updates:" "Upgrade was unsuccessful."
+   fi 
 
 else
-	notify-send "Auto-updates:" "No upgradeable packages."
+   notify-send "Auto-updates:" "No upgradeable packages."
 	
 fi
 
