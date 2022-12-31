0397e0b3ef04517198a7f8a97450c9c1c46703b6
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2022-12-31 21:06:09 +0800

    Update listenotifications.sh

--- listenotifications.sh
+++ listenotifications.sh
@@ -5,66 +5,66 @@
 
 while true
 do
-   NOTIFLOGS=~/Documents/listenotif/notiflogs.txt
-   NOTIFBUF=~/Documents/listenotif/notifbuf.txt
-   EMAIL="cabpacedilla@gmail.com"
-   SKYPE_WIN=$(wmctrl -lp | grep Skype | awk '{print $1}')
-   
-   NOTIF=("@mentioned you" "mentioned all")
-   
+NOTIFLOGS=~/Documents/listenotif/notiflogs.txt
+NOTIFBUF=~/Documents/listenotif/notifbuf.txt
+EMAIL="cabpacedilla@gmail.com"
+SKYPE_WIN=$(wmctrl -lp | grep Skype | awk '{print $1}')
+
+NOTIF=("@mentioned you" "mentioned all")
+
+dbus-monitor "interface='org.freedesktop.Notifications'" |\
+grep --line-buffered "string" |\
+grep --line-buffered -e method -e ":" -e '""' -e urgency -e notify -v |\
+grep --line-buffered '.*(?=string)|(?<=string).*' -oPi |\
+grep --line-buffered -v '^\s*$' |\
+#xargs -d '\n' -I '{}' espeak '{}' \
+xargs -d '\n' -I '{}' \
+printf "---$(date)---\n"{}"\n" > $NOTIFLOGS &
+
+# Switch to Skype every time someone @mention you or @mention all
+while inotifywait -e modify $NOTIFLOGS
+do     
+   # Divert notification monitor update to log file
    dbus-monitor "interface='org.freedesktop.Notifications'" |\
-   grep --line-buffered "string" |\
-   grep --line-buffered -e method -e ":" -e '""' -e urgency -e notify -v |\
-   grep --line-buffered '.*(?=string)|(?<=string).*' -oPi |\
-   grep --line-buffered -v '^\s*$' |\
-   #xargs -d '\n' -I '{}' espeak '{}' \
-   xargs -d '\n' -I '{}' \
-   printf "---$(date)---\n"{}"\n" > $NOTIFLOGS &
-   
-   # Switch to Skype every time someone @mention you or @mention all
-   while inotifywait -e modify $NOTIFLOGS
+      grep --line-buffered "string" |\
+      grep --line-buffered -e method -e ":" -e '""' -e urgency -e notify -v |\
+      grep --line-buffered '.*(?=string)|(?<=string).*' -oPi |\
+      grep --line-buffered -v '^\s*$' |\
+      #xargs -d '\n' -I '{}' espeak '{}'\
+      xargs -d '\n' -I '{}'\
+      printf "---$(date)---\n"{}"\n" > $NOTIFLOGS &
+      # Get first 10 lines from log file and save to buffer text file
+
+   head $NOTIFLOGS > $NOTIFBUF 
+
+   # Read buffer text file for keyword filter
+   while read -r line
    do     
-      # Divert notification monitor update to log file
-      dbus-monitor "interface='org.freedesktop.Notifications'" |\
-         grep --line-buffered "string" |\
-         grep --line-buffered -e method -e ":" -e '""' -e urgency -e notify -v |\
-         grep --line-buffered '.*(?=string)|(?<=string).*' -oPi |\
-         grep --line-buffered -v '^\s*$' |\
-         #xargs -d '\n' -I '{}' espeak '{}'\
-         xargs -d '\n' -I '{}'\
-         printf "---$(date)---\n"{}"\n" > $NOTIFLOGS &
-         # Get first 10 lines from log file and save to buffer text file
-      
-      head $NOTIFLOGS > $NOTIFBUF 
-     
-      # Read buffer text file for keyword filter
-      while read -r line
-      do     
-         for KEYWORD in "${NOTIF[@]}"; do
-            #echo $KEYWORD
-            case "$line" in
-                  *"$KEYWORD"*)
-                  if echo "${NOTIF[*]}" | grep "$KEYWORD"; then
-                     SENDER=$(echo $KEYWORD | awk '{print $1}')
-                     SENDER=$(echo ${SENDER^^})
-                     # Send email copy of notification to email
-            	      mail -s "Notification from $SENDER" "$EMAIL"
-                     # Switch to Skype window
-                     wmctrl -ia "$SKYPE_WIN"   
-                     notify-send "Notification from $SENDER"
-                     break
-                  else
-                     :
-                  fi
-                  ;;                 
-            esac              
-        done
-     done < $NOTIFBUF
-         
-     # Empty text files
-     > $NOTIFLOGS
-     > $NOTIFBUF
-    
-   done  
-   sleep 0.01s
+      for KEYWORD in "${NOTIF[@]}"; do
+         #echo $KEYWORD
+         case "$line" in
+               *"$KEYWORD"*)
+               if echo "${NOTIF[*]}" | grep "$KEYWORD"; then
+                  SENDER=$(echo $KEYWORD | awk '{print $1}')
+                  SENDER=$(echo ${SENDER^^})
+                  # Send email copy of notification to email
+                  mail -s "Notification from $SENDER" "$EMAIL"
+                  # Switch to Skype window
+                  wmctrl -ia "$SKYPE_WIN"   
+                  notify-send "Notification from $SENDER"
+                  break
+               else
+                  :
+               fi
+               ;;                 
+         esac              
+     done
+  done < $NOTIFBUF
+
+  # Empty text files
+  > $NOTIFLOGS
+  > $NOTIFBUF
+
+done  
+sleep 0.01s
 done
