6c87d5a66a2fbc10859f4363726d2021993d6a6e
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2023-01-27 21:12:33 +0800

    Update autobrightness.sh

--- autobrightness.sh
+++ autobrightness.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/bash
 
 BRIGHT_PATH=/sys/class/backlight/amdgpu_bl0/brightness
-BRIGHTNESS=$(cat /sys/class/backlight/amdgpu_bl0/brightness)
-OPTIMAL=55
+BRIGHTNESS=$(cat $BRIGHT_PATH)
+OPTIMAL=80
 
-if [ "$BRIGHTNESS" != "$OPTIMAL" ]; then
-	echo $OPTIMAL | sudo tee /sys/class/backlight/amdgpu_bl0/brightness
-else
-	:
-fi
-
-while inotifywait -e modify $BRIGHT_PATH; do
-	BRIGHTNESS=$(cat /sys/class/backlight/amdgpu_bl0/brightness)
-
+brightness_check()
+{
 	if [ "$BRIGHTNESS" != "$OPTIMAL" ]; then
-		echo $OPTIMAL | sudo tee /sys/class/backlight/amdgpu_bl0/brightness
+		echo $OPTIMAL | sudo tee $BRIGHT_PATH
 	else
 		:
 	fi
+}
 
+brightness_check
+while inotifywait -e modify $BRIGHT_PATH; do
+	brightness_check
 done
