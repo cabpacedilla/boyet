133e713daf011072507846abda2da4e5228ae7b6
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2023-02-19 15:44:45 +0800

    Update cpuTempAlarm.sh

--- cpuTempAlarm.sh
+++ cpuTempAlarm.sh
@@ -6,15 +6,15 @@ while true
 do
 
 TEMPEST=$(sensors | grep Tctl | awk '{print $2}')
-TEMPNUM=${TEMPEST:1:-4}
+TEMPEST=$(echo "$TEMPEST" | awk '{ print substr( $0, 2, length($0)-3 ) }')
 HIGHTEMP=90
 
-if [ "$TEMPNUM" -ge "$HIGHTEMP" ]; then
+if [ -z "$TEMPEST" ]; then
+	continue
+elif (( $(echo "$TEMPEST > $HIGHTEMP" | bc -l) )); then
 	notify-send "CPU temp alert!" "$HIGHTEMP degrees C temp is high. Please check application with high CPU usage."
-	
 else 
 	:
-
 fi
 
 sleep 1 
