b379f31bb302c04cb4e379d0f15440f8c3e4442a
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2023-03-05 06:40:39 +0800

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -6,33 +6,29 @@
 # Add the script in autostart after saving.
 
 #!/usr/bin/bash
-while true
-do
+while true; do
+
+notify-send "Auto-updates:" "Checking updates."
 
 UPGRADEABLE=$(sudo apt update | grep "packages can be upgraded.")
-DATE=$(date | awk '{print $2}')
+#DATE=$(date | awk '{print $2}')
 LIST=~/bin/upgradeable.txt
 
-if [ "$DATE" = "30" ] || [ "$DATE" = "15" ]; then
-if [ -n "$UPGRADEABLE" ]; then
-   sudo apt list --upgradeable | tail -n +2 > $LIST
-   PACKAGES=$(cut -d/ -f1 $LIST)
-   notify-send "Auto-updates:" "$PACKAGES to be updated."	
-   notify-send "Updating $PACKAGES..."
-   if yes | sudo apt upgrade; then
-    	notify-send "Auto-updates:" "$PACKAGES were updated."
-    	yes | sudo apt autoremove
-   else
-    	notify-send "Auto-updates:" "Upgrade was unsuccessful."
-   fi 
+#if [ "$DATE" = "30" ] || [ "$DATE" = "15" ]; then
+if [ -z "$UPGRADEABLE" ]; then
+	notify-send "Auto-updates:" "No upgradeable packages."
+elif [ -n "$UPGRADEABLE" ]; then
+	sudo apt list --upgradeable | tail -n +2 > "$LIST"
+	PACKAGES=$(cut -d/ -f1 "$LIST")
+	notify-send "Auto-updates:" "$PACKAGES to be updated."	
+	notify-send "Updating $PACKAGES..."	
+	if gnome-terminal -- sudo apt upgrade -y && yes | sudo apt autoremove; then
+	   notify-send "Auto-updates:" "$PACKAGES were updated."
+	else
+	   notify-send "Auto-updates:" "Upgrade was unsuccessful."
+	fi 
 else
-   notify-send "Auto-updates:" "No upgradeable packages."
-	
-fi
-
-else
- :
-
+:
 fi
 
 sleep 7h
