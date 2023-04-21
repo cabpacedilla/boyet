be74a2a1ef91d342d5c1b0729c860072520608ef
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2023-04-21 23:03:27 +0800

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -8,32 +8,34 @@
 #!/usr/bin/bash
 while true; do
 
-notify-send "Checking system updates."
-
-UPGRADEABLE=$(sudo apt update | tail -n1)
 LIST=~/bin/upgradeable.txt
 DATE=$(date | awk '{print $2}')
 
-if [ "$DATE" = "30" ] || [ "$DATE" = "15" ]; then	
-	if [ "$UPGRADEABLE" = "All packages are up to date." ]; then
-		notify-send "System is up to date."
-	else 
-		PACKAGES=$(apt list --upgradable | tail -n +2 > "$LIST")
-		PACKAGES=$(cut -d/ -f1 "$LIST")
-		notify-send "$PACKAGES to be updated."	
-		notify-send "Updating $PACKAGES..."	
-		if sudo apt -y upgrade; then
-			notify-send "Auto-removing and auto-cleaning package updates"
-			sudo apt -y autoremove; 
-			sudo apt autoclean
-			notify-send "$PACKAGES were updated."
-		else
-			notify-send "Upgrade was unsuccessful."
-		fi 
+if [ "$DATE" = "30" ] || [ "$DATE" = "21" ]; then
+	notify-send "Auto-updates: Checking system updates."
+	UPGRADES=$(/usr/lib/update-notifier/apt-check |& cut -d";" -f 1 &)
+	if [ "$UPGRADES" -gt 0 ]; then
+	  	sudo apt update
+     		sudo apt list --upgradable | tail -n +2 > "$LIST"
+    		PACKAGES=$(cut -d/ -f1 "$LIST")
+     		if [ -n "$PACKAGES" ]; then
+			notify-send "Auto-updates: $PACKAGES to be updated."   
+			notify-send "Auto-updates: Updating $PACKAGES..."   
+			if sudo apt -y upgrade; then
+				notify-send "Auto-updates: Auto-removing and auto-cleaning package updates"
+				sudo apt -y autoremove;
+				sudo apt autoclean
+				notify-send "Auto-updates: $PACKAGES were updated."
+			else
+				notify-send "Auto-updates: Upgrade was unsuccessful."
+			fi
+	  	fi
+	else
+		notify-send "Auto-updates: System is up to date."
 	fi
 else
-	notify-send "Date is not 15 or 30"
+	 notify-send "Auto-updates: Date is not 15 or 30"
 fi
 
-sleep 7h
+sleep 2h
 done
