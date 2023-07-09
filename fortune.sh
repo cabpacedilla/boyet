2523982e8eccc88e3a407faa3dd07e46fab9ef4f
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2023-07-10 04:48:25 +0800

    Update fortune.sh

--- fortune.sh
+++ fortune.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 while true; do
 
+select_random() {
+    printf "%s\0" "$@" | shuf -z -n1 | tr -d '\0'
+}
+
 # Find fortune file randomly
-FORTUNEFILE=$(find ~/Documents/claive/fortune | shuf -n 1 &)
+FORTUNEFILE=$(find ~/Documents/claive/fortune | shuf -n 1)
 
 # Put the quotes in an array by using '%' delimiter in the quotes to separate the items 
 declare -a ARR=(); 
 readarray -td% ARR < "$FORTUNEFILE" 
 
 # Get a quote randomly
-RANDFORTUNE=${ARR[$RANDOM % ${#ARR[@]}]}
+#RANDFORTUNE=${ARR[$RANDOM % ${#ARR[@]}]}
+RANDFORTUNE=$(select_random "${ARR[@]}")
 
 if [ -z "${RANDFORTUNE}" ]; then
 	continue
