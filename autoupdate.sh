ad93f57dc93d035426e4d235b46bbd0d70183b09
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2023-08-15 09:52:29 +0800

    Update autoupdate.sh

--- autoupdate.sh
+++ autoupdate.sh
@@ -12,29 +12,29 @@ LIST=~/bin/upgradeable.txt
 DATE=$(date | awk '{print $2}')
 
 if [ "$DATE" = "15" ] || [ "$DATE" = "30" ]; then
-	notify-send --app-name "Auto-updates: Checking system updates."
+	notify-send --app-name "Auto-updates:" "Checking system updates."
 	UPGRADES=$(/usr/lib/update-notifier/apt-check |& cut -d";" -f 1 &)
 	if [ "$UPGRADES" -gt 0 ]; then
 	  	sudo apt update
      		sudo apt list --upgradable | tail -n +2 > "$LIST"
     		PACKAGES=$(cut -d/ -f1 "$LIST")
      		if [ -n "$PACKAGES" ]; then
-			notify-send --app-name "Auto-updates: $PACKAGES to be updated."   
-			notify-send --app-name "Auto-updates: Updating $PACKAGES..."   
+			notify-send --app-name "Auto-updates:" "$PACKAGES to be updated."   
+			notify-send --app-name "Auto-updates:" "Updating $PACKAGES..."   
 			if sudo apt -y upgrade; then
-				notify-send --app-name "Auto-updates: Auto-removing and auto-cleaning package updates"
+				notify-send --app-name "Auto-updates:" "Auto-removing and auto-cleaning package updates"
 				sudo apt -y autoremove;
 				sudo apt autoclean
-				notify-send --app-name "Auto-updates: $PACKAGES were updated."
+				notify-send --app-name "Auto-updates:" "$PACKAGES were updated. System is up to date."
 			else
-				notify-send --app-name "Auto-updates: Upgrade was unsuccessful."
+				notify-send --app-name "Auto-updates:" "Upgrade encountered an error."
 			fi
 	  	fi
 	else
-		notify-send --app-name "Auto-updates: System is up to date."
+		notify-send --app-name "Auto-updates:" "System is already up to date."
 	fi
 else
-	 notify-send "Auto-updates: Date is not 15 or 30"
+	 notify-send "Auto-updates:" "Date is not 15 or 30"
 fi
 
 sleep 2h
