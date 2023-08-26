fca378810cea264b18b1cc29889be1e290ec6592
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2023-08-26 13:38:45 +0800

    Update preventscreensaver.sh

--- preventscreensaver.sh
+++ preventscreensaver.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 while true; do
 
-PLAYING=$(sudo cat /proc/asound/card1/pcm0p/sub0/status | grep -o "RUNNING" &)
+PLAYING=$(sudo cat /proc/asound/card1/pcm0p/sub0/status | grep -o "RUNNING")
 if [ "$PLAYING" = "RUNNING" ]; then
 	xscreensaver-command -deactivate
 fi
