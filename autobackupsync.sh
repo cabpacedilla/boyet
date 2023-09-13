518d1856f1c1bf6c94c52498285eed501db86f51
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2023-09-13 23:07:20 +0800

    Update autobackupsync.sh

--- autobackupsync.sh
+++ autobackupsync.sh
@@ -5,7 +5,7 @@
 
 # Needs filesystem change monitoring tool like inotify in Linux
 
-while inotifywait modify-r -e modify,create,delete ~/<sourcefolder>
+while inotifywait -r -e modify,create,delete ~/<sourcefolder>
 do
    notify-send "Folder updated. Syncing folder."
    sudo rsync -avHAX ~/<sourcefolder>/ /<destinationfolder> --delete
