517f3ac3badc6053b9b22487b1e973088c4db98f
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2023-11-07 10:07:29 +0800

    Update notifWatchMacos.sh

--- notifWatchMacos.sh
+++ notifWatchMacos.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-#while true; do
+while true; do
 # Set user in /etc/sudoers file without providing password with rsync command
 # add line <username> ALL=(ALL) NOPASSWD: /usr/bin/rsync
 
 # Needs filesystem change monitoring tool like inotify in Linux
-SRC_DIR="/private/var/root"
-#SRC_DIR="/private/var/folders/ql/6lc79_dd3t58x2zm3v9qzv040000gn/T/TemporaryItems"
+#SRC_DIR="/private/var/root"
+SRC_DIR="/private/var/folders/ql/6lc79_dd3t58x2zm3v9qzv040000gn/T/.LINKS"
 SRC_FILE="/private/var/folders/ql/6lc79_dd3t58x2zm3v9qzv040000gn/0/com.apple.notificationcenter/db2/db-wal"
 TIME=$(date +"%I:%M %p")
 
@@ -19,6 +19,6 @@ while sudo /opt/local/bin/fswatch "$SRC_DIR" | osascript -e "display notificatio
 	    osascript -e "display notification \"$RECEIVED_MESSAGE.\" with title \"Message notification:    $TIME\""
 	fi
 done
-#sleep 0.01s
+sleep 0.01s
 
-#done
+done
