a8d9f2bdf51ceecf5a49a5569f154ad49b7c3168
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2024-01-22 01:27:00 +0800

    Update laptopLid_close.sh

--- laptopLid_close.sh
+++ laptopLid_close.sh
@@ -22,13 +22,13 @@ while true; do
 LID_PATH=/proc/acpi/button/lid/LID0/state
 
 ## 1. Set for open state
-OPEN_STATE="open"
+STATE="open"
    
 ## 2. Get laptop lid state
 LID_STATE=$(less $LID_PATH | awk '{print $2}')
 
 ## 3. Do nothing if lid is open
-if [ "$LID_STATE" = "$OPEN_STATE" ]; then
+if [ "$LID_STATE" = "$STATE" ]; then
 	:	  
 ## 4. Suspend if lid is closed
 else 
