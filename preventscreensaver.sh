5365189f41cd44145d065dc8b45e204ac8022594
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2024-02-01 12:21:39 +0800

    Update preventscreensaver.sh

--- preventscreensaver.sh
+++ preventscreensaver.sh
@@ -2,7 +2,7 @@
 while true; do
 
 #PROCSOUND=$(sudo cat /proc/asound/card1/pcm0p/sub0/status | grep -o "RUNNING")
-MEDIA_PLAY=$(pacmd list-sink-inputs | grep -w "START_CORKED" | awk '{ print $2 }')
+MEDIA_PLAY=$(pacmd list-sink-inputs | grep -w "RUNNING" | awk '{ print $2 }')
 if [ -n "$MEDIA_PLAY" ]; then
 	xscreensaver-command -deactivate
 else
