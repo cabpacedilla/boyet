239a549942144de07d7cb88b317fdcd11751e2bc
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2024-05-27 16:38:48 +0800

    Update recentFiles.sh

--- recentFiles.sh
+++ recentFiles.sh
@@ -4,9 +4,11 @@ while true; do
 # Create a shortcut key with command qterminal -e recentFiles.sh
 
 RECENT_LIST=~/bin/recentFiles.txt
-RECENTLY_USED=~/.local/share/recently-used.xbel
+REVERSE_LIST=~/bin/reverseRecent.txt
+RECENTLY_FILE=~/.local/share/recently-used.xbel
 
-RECENT_FILES=$(awk -F"file://|\" " '/file:\/\// {print $2}' "$RECENTLY_USED")
+RECENT_FILES=$(awk -F"file://|\" " '/file:\/\// {print $2}' "$RECENTLY_FILE")
+RECENT_FILES_CLEAN=$(awk -F"file://|\" " '/file:\/\// {print $2}' "$RECENTLY_FILE" | sed 's/%20/\ /g')
 
 echo "$RECENT_FILES" > "$RECENT_LIST"
 
@@ -21,15 +23,19 @@ done < "$RECENT_LIST"
 
 #echo "${RECENTARR[@]}"
 
-cat "$RECENT_LIST" | tail 
+echo "$RECENT_FILES_CLEAN" | tail > "$REVERSE_LIST"
+REVERSE_LIST=$(nl "$REVERSE_LIST")
+echo "$REVERSE_LIST"
 
 echo "Please provide the sequence number of the accessed file: "
 
 read OPEN_FILE
 
-file=$(echo "${RECENTARR[$OPEN_FILE]}" | sed 's/%20/\\ /g' | sed 's/%2520/\\ /g')
+file=$(echo "${RECENTARR[${OPEN_FILE} - 1]}" | sed 's/%20/\\ /g' | sed 's/%2520/\\ /g')
 file=$(echo "$file" | xargs)
 
 xdg-open "$file"
 
 done
+
+
