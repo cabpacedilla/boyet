5649bc7cb35ddcac07033c85f92de82ea7966eab
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2024-06-03 08:04:27 +0800

    Update autobackupsync.sh

--- autobackupsync.sh
+++ autobackupsync.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/sh
-
 # Set user in /etc/sudoers file without providing password with rsync command
-# add line <username> ALL=(ALL) NOPASSWD: /usr/bin/rsync at the bottom of the /etc/sudoers file using sudo visudo
-# Add /usr/bin/sudo if destination need superuser privilege
-
-# Needs filesystem change monitoring tool like inotify in Linux
-
-# while inotifywait -r -e modify,create,delete ~/<sourcefolder> ~/<sourcefolder> 
-while inotifywait -r -e modify,create,delete /home/boyet/Documents/claive/ /home/boyet/bin/
+# add line <username> ALL=(ALL) NOPASSWD: /usr/bin/rsync
+#while inotifywait -e modify,create /home/capa/Documents/ /home/capa/bin/
+while inotifywait -r -e modify,create /home/capa/Documents/ /home/capa/bin/
 do
-   notify-send "Folder updated. Syncing folder."
-	#rsync -avl /home/boyet/Documents/claive/ /media/boyet/Data/claive --delete
- 	sudo rsync -avhP /home/boyet/Documents/claive/ /home/boyet/bin/ /media/boyet/Data/claive
+	TIME=$(date +"%I:%M %p")
+	#notify-send --app-name "Auto-backup:    $TIME" "Folder updated. Syncing folder."
+	#rsync -avHAXS /home/boyet/Documents/claive/ /home/boyet/Documents/backup
+	#rsync -avz /home/boyet/Documents/claive/ /home/boyet/Documents/backup --delete
+	#rsync -avuhP /home/boyet/Documents/claive/ /home/boyet/bin/ /media/boyet/Data/claive
+	sudo rsync -avuhPHAXS /home/capa/Documents /home/capa/bin /run/media/capa/sda1/claive
 	if [ $? = 0 ]; then
-		notify-send "Backup sync was successful."
+		notify-send --app-name "Auto-backup:    $TIME" "Backup sync was successful."
 	else
-		notify-send "Backup sync encountered an error."
+		notify-send --app-name "Auto-backup:    $TIME""Backup sync encountered an error."
 	fi
+   #sudo rsync -avHAX ~/Documents/testfiles/ /mnt/backup/ --delete
+   #sudo tar cvf /mnt/backup/tarball$DATE ~/Documents/testfiles/*
+
 done
