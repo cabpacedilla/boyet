ebb30dbb6f76eaacb3554d8c0d67f7780472a603
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2024-07-14 11:26:36 +0800

    Update autobackupMacos.sh

--- autobackupMacos.sh
+++ autobackupMacos.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+# This script will automatically backup when source files are changed
+# This script was assembled written by Claive Alvin P. Acedilla. It can be copied, modified and redistributed.
 # Set user in /etc/sudoers file without providing password with rsync command
 # add line <username> ALL=(ALL) NOPASSWD: /usr/bin/rsync
 
 # Needs filesystem change monitoring tool like inotify in Linux
-SRC_DIR="/Users/mini001/Documents/Claive/"
-DST_DIR="/Users/mini001/Documents/backup_claive"
+SRC_DIR="/Users/macPC/Documents/Claive/"
+DST_DIR="/Users/macPC/Documents/backup_claive"
 
 while /opt/local/bin/fswatch -o -r "$SRC_DIR" | osascript -e "display notification \"Watching folder for changes.\" with title \"Backup sync:    $TIME\""; do 
 #xargs -n1 -I{} rsync -avl "$SRC_DIR"  "$DST_DIR"; 
