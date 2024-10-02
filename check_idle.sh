9f8765ade59ad0b7d5ee194472a8e8ac27e381a0
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2024-10-02 23:16:54 +0800

    Update check_idle.sh

--- check_idle.sh
+++ check_idle.sh
@@ -2,10 +2,10 @@
 # Threshold for CPU usage (in percent) to consider a process as consuming resources
 CPU_THRESHOLD=10
 
-# Get idle CPU percentage using `sar`
-IDLE_CPU=$(sar 1 1 | grep 'Average' | awk '{print $NF}')
-
 while true; do
+	# Get idle CPU percentage using `sar`
+	IDLE_CPU=$(sar 1 1 | grep 'Average' | awk '{print $NF}')
+
 	# Check if the system is considered idle (you can adjust the threshold)
 	if (( $(echo "$IDLE_CPU > 90" | bc -l) )); then
 	    # List processes consuming more than the threshold CPU usage
