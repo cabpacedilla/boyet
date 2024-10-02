da89bc0467a12c3355f64e79fc3aae3c3508389d
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2024-10-02 23:12:53 +0800

    Update check_idle.sh

--- check_idle.sh
+++ check_idle.sh
@@ -6,15 +6,15 @@ CPU_THRESHOLD=10
 IDLE_CPU=$(sar 1 1 | grep 'Average' | awk '{print $NF}')
 
 while true; do
-# Check if the system is considered idle (you can adjust the threshold)
-if (( $(echo "$IDLE_CPU > 90" | bc -l) )); then
-    # List processes consuming more than the threshold CPU usage
-	notify-send --app-name "Idle power hog:" "Processes consuming more than $CPU_THRESHOLD% CPU:"
-	ps --sort=-%cpu -eo pid,%cpu,comm | awk -v threshold="$CPU_THRESHOLD" '$2 > threshold {print $0}'
-else
-    :
-fi
-
-sleep 0.1s
+	# Check if the system is considered idle (you can adjust the threshold)
+	if (( $(echo "$IDLE_CPU > 90" | bc -l) )); then
+	    # List processes consuming more than the threshold CPU usage
+		notify-send --app-name "Idle power hog:" "Processes consuming more than $CPU_THRESHOLD% CPU:"
+		ps --sort=-%cpu -eo pid,%cpu,comm | awk -v threshold="$CPU_THRESHOLD" '$2 > threshold {print $0}'
+	else
+	    :
+	fi
+	
+	sleep 0.1s
 done
 
