37c15c73dbde44a95a184d6b34f5084b8f3c2328
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2024-11-12 22:54:54 +0800

    Update check_idle.sh

--- check_idle.sh
+++ check_idle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Threshold for CPU usage (in percent) to consider a process as consuming resources
-CPU_THRESHOLD=70
+CPU_THRESHOLD=10
 
 while true; do
     # Get idle CPU percentage using `sar`
@@ -9,8 +9,8 @@ while true; do
     # Calculate CPU usage by subtracting idle CPU from 100
     CPU_USAGE=$(echo "100 - $IDLE_CPU" | bc)
 
-    # Check if CPU usage is above the threshold (i.e., system is under load)
-    if (( $(echo "$CPU_USAGE > $CPU_THRESHOLD" | bc -l) )); then
+    # Check if CPU usage is below the threshold (i.e., system is under load)
+    if (( $(echo "$CPU_USAGE < $CPU_THRESHOLD" | bc -l) )); then
         # List processes consuming more than the threshold CPU usage
         konsole -e bash -c "echo -e \"Top 10 CPU Consumers:\n\$(ps --sort=-%cpu -eo pid,%cpu,comm | head -n 11)\n\"; read -p 'Press enter to close...'" &
     else
@@ -19,5 +19,5 @@ while true; do
     fi
 
     # Wait for 1 minute before checking again
-    sleep 1m
+    sleep 10
 done
