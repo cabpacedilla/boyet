db606603c5f9f739cdf069671bfd14e3c42c4f81
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2025-01-09 22:33:44 +0800

    Update preventscreensaver.sh

--- preventscreensaver.sh
+++ preventscreensaver.sh
@@ -2,16 +2,16 @@
 
 # Function to check if media is playing
 is_media_playing() {
-	#~ MEDIA_PLAY=$(pacmd list-sink-inputs | grep -w "RUNNING" | awk '{ print $2 }')
+	#MEDIA_PLAY=$(pacmd list-sink-inputs | grep -w "RUNNING" | awk '{ print $2 }')
+    local MEDIA_PLAY
     MEDIA_PLAY=$(pactl list | grep -w "RUNNING" | awk '{ print $2 }')
+    echo "$MEDIA_PLAY"
 }
 
 # Main loop
 while true; do
-    MEDIA_PLAY=$(is_media_playing)
-
-    if [ $? -eq 0 ]; then
-        if [ -n "$MEDIA_PLAY" ]; then
+    if MEDIA_PLAY=$(is_media_playing); then
+        if [[ -n "$MEDIA_PLAY" ]]; then
             xscreensaver-command -deactivate
         else
 			:
@@ -22,4 +22,3 @@ while true; do
 
     sleep 0.1
 done
-
