0f5fae8ba1a1a5c47edafb3788733b9215c60f2a
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2025-02-06 06:18:01 +0800

    Update autobackupsync.sh

--- autobackupsync.sh
+++ autobackupsync.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/sh
 # Set user in /etc/sudoers file without providing password with rsync command
 # add line <username> ALL=(ALL) NOPASSWD: /usr/bin/rsync
-#while inotifywait -e modify,create /home/capa/Documents/ /home/capa/bin/
-while inotifywait -r -e modify,create /home/capa/Documents/ /home/capa/bin/
-do
+ORIGIN=$HOME/Documents/
+DESTINATION=/run/media/claiveapa/Data/claive/Documents/nobara/
+while inotifywait -r -e modify,create $ORIGIN; do
 	TIME=$(date +"%I:%M %p")
-	#notify-send --app-name "Auto-backup:    $TIME" "Folder updated. Syncing folder."
-	#rsync -avHAXS /home/boyet/Documents/claive/ /home/boyet/Documents/backup
-	#rsync -avz /home/boyet/Documents/claive/ /home/boyet/Documents/backup --delete
-	#rsync -avuhP /home/boyet/Documents/claive/ /home/boyet/bin/ /media/boyet/Data/claive
-	sudo rsync -avuhPHAXS /home/capa/Documents /home/capa/bin /run/media/capa/sda1/claive
+	rsync -avz --protect-args "$ORIGIN" "$DESTINATION"
 	if [ $? = 0 ]; then
 		notify-send --app-name "Auto-backup:    $TIME" "Backup sync was successful."
 	else
@@ -17,5 +13,4 @@ do
 	fi
    #sudo rsync -avHAX ~/Documents/testfiles/ /mnt/backup/ --delete
    #sudo tar cvf /mnt/backup/tarball$DATE ~/Documents/testfiles/*
-
 done
