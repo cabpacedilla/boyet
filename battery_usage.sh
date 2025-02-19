6c7546e410add50c8f2548b66ee488f1edbe44f1
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2025-02-19 09:19:28 +0800

    Update battery_usage.sh

--- battery_usage.sh
+++ battery_usage.sh
@@ -1,3 +1,17 @@
+#!/usr/bin/bash
+# This script will alert the power consumption in a given time
+# This script was assembled written by Claive Alvin P. Acedilla. It can be copied, modified and redistributed.
+# January 2025
+
+# Steps for the task:
+# 1. Create a bin directory inside your home directory
+# 2. Change directory to the bin directory
+# 3. Create the bash script file below with nano or gedit and save it with a filename like lowMemAlert.sh
+# 4. Make file executable with chmod +x lowMemAlert.sh command
+# 5. Add the lowMemAlert.sh command in Startup applications
+# 6. Reboot the laptop
+# 7. Log in and simulate low memory scenario by running many high memory consuming processes until free memory space reaches desired low free memory space in megabytes
+# 8. Low memory alert message will be displayed
 #!/bin/bash
 
 # Start the monitoring
