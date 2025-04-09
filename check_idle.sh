965388b92ffa28667eee5c86de3a67fcfd2a0d06
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2025-04-09 22:27:28 +0800

    Update check_idle.sh

--- check_idle.sh
+++ check_idle.sh
@@ -27,30 +27,36 @@ check_resources() {
     local current_time
 
     cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk -F'id,' '{print $1}' | awk '{print 100 - $NF}')
-    mem_usage=$(free -m | awk 'NR==2{printf "%.2f", $3*100/$2 }')
+    mem_usage=$(free -m | awk 'NR==2{printf "%.2f\n", $3*100/$2 }')
     disk_io=$(iostat -m 1 2 | awk 'NR==15 {print $3+$4}')
     current_time=$(date +"%Y-%m-%d %H:%M:%S")
 
     if ! is_media_playing; then
         if (( $(echo "$cpu_usage > $CPU_THRESHOLD" | bc -l) )); then
-            notify-send "Abnormal CPU Usage" "$current_time - Abnormal CPU usage: $cpu_usage%"
-            echo "$current_time - Abnormal CPU usage: $cpu_usage%" | sudo tee -a "$LOG_FILE" > /dev/null
-            ps aux --sort=-%cpu | head -n 10 >> "$LOG_FILE"
+            notify-send "Abnormal CPU Usage" "$current_time - Abnormal CPU usage: $cpu_usage%" &
+            echo "$current_time - Abnormal CPU usage: $cpu_usage%" | sudo tee -a "$LOG_FILE" > /dev/null &
+            ps aux --sort=-%cpu | head -n 10 >> "$LOG_FILE" &
+        else
+            :
         fi
 
         if (( $(echo "$mem_usage > $MEMORY_THRESHOLD" | bc -l) )); then
-            notify-send "Abnormal Memory Usage" "$current_time - Abnormal memory usage: $mem_usage%"
-            echo "$current_time - Abnormal CPU usage: $mem_usage%" | sudo tee -a "$LOG_FILE" > /dev/null
-            ps aux --sort=-%mem | head -n 10 >> "$LOG_FILE"
+            notify-send "Abnormal Memory Usage" "$current_time - Abnormal memory usage: $mem_usage%" &
+            echo "$current_time - Abnormal CPU usage: $mem_usage%" | sudo tee -a "$LOG_FILE" > /dev/null &
+            ps aux --sort=-%mem | head -n 10 >> "$LOG_FILE" &
+        else
+            :
         fi
 
         if (( $(echo "$disk_io > $DISK_IO_THRESHOLD" | bc -l) )); then
-            notify-send "Abnormal Disk I/O" "$current_time - Abnormal Disk I/O: $disk_io MB/s"
-            echo "$current_time - Abnormal CPU usage: $disk_io%" | sudo tee -a "$LOG_FILE" > /dev/null
-            sudo iotop -boP -n 1 | sudo tee -a "$LOG_FILE" > /dev/null
+            notify-send "Abnormal Disk I/O" "$current_time - Abnormal Disk I/O: $disk_io MB/s" &
+            echo "$current_time - Abnormal CPU usage: $disk_io%" | sudo tee -a "$LOG_FILE" > /dev/null &
+            sudo iotop -boP -n 1 | sudo tee -a "$LOG_FILE" > /dev/null &
+        else
+            :
         fi
     else
-        'echo active > /tmp/sway_idle_status'
+        echo active > /tmp/sway_idle_status
     fi
 }
 
@@ -62,12 +68,13 @@ start_swayidle() {
 
 # Check if the system is idle by reading the status file
 check_idle_status() {
-    if test -f "$IDLE_STATUS_FILE"; then
-        idle_status=$(cat "$IDLE_STATUS_FILE")
-        if [[ "$idle_status" == "idle" ]]; then
-            echo "System is idle. Checking resources..."
-            check_resources
-        fi
+    #if test -f "$IDLE_STATUS_FILE"; then
+    idle_status=$(cat "$IDLE_STATUS_FILE")
+    if [[ "$idle_status" == "idle" ]]; then
+        echo "System is idle. Checking resources..."
+        check_resources
+    else
+        :
     fi
 }
 
