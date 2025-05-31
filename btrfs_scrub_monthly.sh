0a94299dfcf579be4156eea5926134cf22427870
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2025-05-31 18:57:30 +0800

    Create btrfs_scrub_monthly.sh

---
+++ btrfs_scrub_monthly.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while true; do
+  # Run only on the 15th day of the month
+  if [ "$(date +%d)" != "15" ]; then
+    sleep 86400
+    continue
+  fi
+
+  LOGFILE="$HOME/scriptlogs/btrfs-scrub-$(date +%Y-%m-%d).log"
+
+  echo -e "\nStarting Btrfs scrub on / at $(date)" | tee -a "$LOGFILE"
+  sudo btrfs scrub start -Bd / >> "$LOGFILE" 2>&1
+  echo "Btrfs scrub completed at $(date)" | tee -a "$LOGFILE"
+
+  echo -e "\nBtrfs filesystem usage at $(date)" | tee -a "$LOGFILE"
+  sudo btrfs filesystem usage / | tee -a "$LOGFILE"
+
+
+  # Sleep for 24 hours to avoid re-running the scrub on the same day
+  sleep 86400
+done
