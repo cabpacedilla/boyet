e7b9236006de38f1bd08ab3ab5bdfd5c2773ff52
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2025-06-11 06:04:01 +0800

    Update autobrightness.sh

--- autobrightness.sh
+++ autobrightness.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/bash
 
 BRIGHT_PATH=/sys/class/backlight/amdgpu_bl0/brightness
-OPTIMAL=128
+#OPTIMAL=200 # for ubuntu
+OPTIMAL=39321
 
 brightness_check()
 {
 	BRIGHTNESS=$(cat $BRIGHT_PATH)
 	if [ "$BRIGHTNESS" != "$OPTIMAL" ]; then
-		sudo echo $OPTIMAL | sudo tee $BRIGHT_PATH
+		brightnessctl --device=amdgpu_bl0 set 60%
 	else
 		:
 	fi
 }
 
+brightnessctl --device=amdgpu_bl0 set 60%
 brightness_check
 while inotifywait -e modify $BRIGHT_PATH; do
 	brightness_check
