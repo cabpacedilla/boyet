bd844a5c131baa5260f3d4ce8997a91fe7dfe65f
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2025-06-19 04:39:17 +0800

    Create backlisten.sh

---
+++ backlisten.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+while true; do
+
+MIN_ID=1
+NO_ID=0
+
+CHECK_SERVICES_IDS=$(pgrep -c "checkservices")
+CHECK_SERVICES_PROC=$(pidof -x "checkservices.sh")
+
+if [ "$CHECK_SERVICES_IDS" -gt $MIN_ID ]; then
+   declare -a CHECK_SERVICESARR
+   IFS=' ' read -r -a CHECK_SERVICESARR <<< "$CHECK_SERVICES_PROC"
+   i=0
+	while [ "${CHECK_SERVICESARR[$i]}" != "${CHECK_SERVICESARR[-1]}" ]; do
+	   echo "${CHECK_SERVICESARR[$i]}"
+		kill "${CHECK_SERVICESARR[$i]}"
+		notify-send -t 10000 --app-name "Check services:" "checkservices instance is already running."
+		i=$((i + 1))
+	done
+
+#elif [ "$CHECK_SERVICES_IDS" -eq $NO_ID ] && [ -z "$CHECK_SERVICES_PROC" ]; then
+elif [ "$CHECK_SERVICES_IDS" -eq $NO_ID ]; then
+	notify-send --app-name "Check services:" "checkservices is not running. Please check if checkservices process is running"
+	if ./checkservices.sh & then
+		notify-send -t 10000 --app-name "Check idle:" "checkservices is running"
+	fi
+else
+	:
+fi
+
+sleep 1s
+
+done
+
