7d3ada41caaee91df0284bc65bd27aec47f3dd14
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2025-06-19 04:38:28 +0800

    Update checkservices.sh

--- checkservices.sh
+++ checkservices.sh
@@ -85,7 +85,7 @@ while true; do
 while [ "$SCRIPTS_CTR" -lt "${#SCRIPTS[@]}" ] ; do
 	# Count number of processes of the script and the process IDs of the scripts
 	SCRIPT_NAME=$(basename "${SCRIPTS[$SCRIPTS_CTR]}")
-	SCRIPT=$(command -v "${SCRIPT_NAME}")
+	SCRIPT=$(command -v "${SCRIPT_NAME}.sh")
 	IDS=$(pgrep -c "$SCRIPT_NAME")
 	PROCS=$(pidof -x "$SCRIPT")
 
@@ -114,4 +114,3 @@ done
 
 sleep 1s
 done
-
