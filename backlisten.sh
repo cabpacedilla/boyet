43c6f760f8b559417ea16828cd30fb0ebc5d3983
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2025-06-20 05:00:20 +0800

    Update backlisten.sh

--- backlisten.sh
+++ backlisten.sh
@@ -4,12 +4,12 @@ while true; do
 MIN_ID=1
 NO_ID=0
 
-CHECK_SERVICES_IDS=$(pgrep -fcx "checkservices")
-CHECK_SERVICES_PROC=$(pgrep -fx "checkservices.sh")
+IDS=$(pgrep -c "checkservices")
+PROCS=$(pidof -x "checkservices.sh")
 
-if [ "$CHECK_SERVICES_IDS" -gt $MIN_ID ]; then
+if [ "$IDS" -gt "$MIN_ID" ]; then
    declare -a CHECK_SERVICESARR
-   IFS=' ' read -r -a CHECK_SERVICESARR <<< "$CHECK_SERVICES_PROC"
+   IFS=' ' read -r -a CHECK_SERVICESARR <<< "$PROCS"
    i=0
 	while [ "${CHECK_SERVICESARR[$i]}" != "${CHECK_SERVICESARR[-1]}" ]; do
 	   echo "${CHECK_SERVICESARR[$i]}"
@@ -19,10 +19,12 @@ if [ "$CHECK_SERVICES_IDS" -gt $MIN_ID ]; then
 	done
 
 #elif [ "$CHECK_SERVICES_IDS" -eq $NO_ID ] && [ -z "$CHECK_SERVICES_PROC" ]; then
-elif [ "$CHECK_SERVICES_IDS" -eq $NO_ID ]; then
+elif [ "$IDS" -eq "$NO_ID" ]; then
 	notify-send --app-name "Check services:" "checkservices is not running. Please check if checkservices process is running"
-	if ./checkservices.sh & then
-		notify-send -t 10000 --app-name "Check idle:" "checkservices is running"
+	SCRIPT_PATH="$HOME/Documents/bin/${SCRIPT_NAME}.sh"
+	if [ -x "$SCRIPT_PATH" ]; then
+		"$SCRIPT_PATH" &
+		notify-send -t 10000 --app-name "Check services:" "checkservices is running"
 	fi
 else
 	:
