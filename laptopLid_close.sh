9ab2d807f8c4f5044c6c20fe6fadcd6346a7ad20
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2025-06-20 12:25:59 +0800

    Update laptopLid_close.sh

--- laptopLid_close.sh
+++ laptopLid_close.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 # This script will activate screen lock when the laptop lid will be closed for auto lid close security in icewm window manager in Linux
 # This script was written by Claive Alvin P. Acedilla. It can be copied, modified and redistributed.
 # September 2020
