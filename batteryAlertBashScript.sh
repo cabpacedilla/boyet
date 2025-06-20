876605eb6fa27c1f01a609386b9aaaa95758b486
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2025-06-20 12:26:15 +0800

    Update batteryAlertBashScript.sh

--- batteryAlertBashScript.sh
+++ batteryAlertBashScript.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 # This script will alert when battery level is below or equal 40% and will notify when battery level is above or equal 80% to optimize laptop battery life.
 # This script was assembled and written by Claive Alvin P. Acedilla. It can be copied, modified and redistributed.
 # September 2020
