9e2aacfabe3536c6cd5b6db106f9433a25dbfb81
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2025-07-23 05:19:07 +0800

    Update btrfs_scrub_monthly.sh

--- btrfs_scrub_monthly.sh
+++ btrfs_scrub_monthly.sh
@@ -10,8 +10,10 @@ while true; do
   LOGFILE="$HOME/scriptlogs/btrfs-scrub-$(date +%Y-%m-%d).log"
 
   echo -e "\nStarting Btrfs scrub on / at $(date)" | tee -a "$LOGFILE"
+  notify-send "Btrfs Scrub" "Starting Btrfs scrub on / at $(date)"
   sudo btrfs scrub start -Bd / >> "$LOGFILE" 2>&1
   echo "Btrfs scrub completed at $(date)" | tee -a "$LOGFILE"
+  notify-send "Btrfs Scrub" "Btrfs scrub completed at $(date)"
 
   echo -e "\nBtrfs filesystem usage at $(date)" | tee -a "$LOGFILE"
   sudo btrfs filesystem usage / | tee -a "$LOGFILE"
