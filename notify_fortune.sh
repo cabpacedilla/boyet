19b2ca5077755f2d4b6ded71b6f65536cdf5c50b
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2025-08-03 05:24:13 +0800

    Update notify_fortune.sh

--- notify_fortune.sh
+++ notify_fortune.sh
@@ -4,7 +4,7 @@ while true; do
 # Alert the random quote
 notify-send -u critical --app-name "Fortune:" "$(fortune)"
 s
-# Sleep in random time
+# Sleep in random seconds
 sleep "$(shuf -i1200-1500 -n1)"
 
 done
