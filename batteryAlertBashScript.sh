964e7935d439190c2b48896870ac6ba28229e635
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2025-08-03 05:07:57 +0800

    Update batteryAlertBashScript.sh

--- batteryAlertBashScript.sh
+++ batteryAlertBashScript.sh
@@ -49,8 +49,7 @@ LOW_BATT=20
 HIGH_BATT=80
 FULL_BATT=100
 BRIGHTNESS=$(cat /sys/class/backlight/amdgpu_bl0/brightness)
-BRIGHT_PATH=/sys/class/backlight/amdgpu_bl0/brightness
-OPTIMAL=4095
+OPTIMAL=52428
 
 #1. Get battery level and state
 BATT_LEVEL=$(acpi -b | grep -P -o '[0-9]+(?=%)')
@@ -63,7 +62,8 @@ if [ "$BATT_LEVEL" -le "$LOW_BATT" ] && [ "$BATT_STATE" = "Discharging," ]; then
 #3. If battery level is 40 or less and charging, do nothing
 elif { [ "$BATT_LEVEL" -le "$LOW_BATT" ] && [ "$BATT_STATE" = "Charging," ]; } || { [ "$BATT_LEVEL" -le "$LOW_BATT" ] && [ "$BATT_STATE" = "Unknown," ]; }; then
 	if [ "$BRIGHTNESS" != "$OPTIMAL" ]; then
-		echo $OPTIMAL | sudo tee $BRIGHT_PATH
+      :
+#     brightnessctl --device=amdgpu_bl0 set 80%
 	fi
 	:
    
@@ -74,7 +74,8 @@ elif { [ "$BATT_LEVEL" -ge "$HIGH_BATT" ] && [ "$BATT_STATE" = "Charging," ]; } 
 #5. If battery level is 80 or less and discharging, do nothing   
 elif { [ "$BATT_LEVEL" -le "$HIGH_BATT" ] &&  [ "$BATT_STATE" = 'Discharging,' ]; } || { [ "$BATT_LEVEL" -gt "$HIGH_BATT" ] &&  [ "$BATT_STATE" = 'Discharging,' ]; }; then
 	if [ "$BRIGHTNESS" != "$OPTIMAL" ]; then
-		echo $OPTIMAL | sudo tee $BRIGHT_PATH
+      :
+#     brightnessctl --device=amdgpu_bl0 set 80%
 	fi
 	:
 ##5. If battery level is 80 or more and discharging, do nothing 
