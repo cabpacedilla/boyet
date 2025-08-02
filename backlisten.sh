d1a96c2f02279aeb71241b9c70986295f898b3fa
Claive Alvin "Boyet" P. Acedilla <cabpacedilla@gmail.com>
2025-08-03 05:03:11 +0800

    Update backlisten.sh

--- backlisten.sh
+++ backlisten.sh
@@ -6,24 +6,24 @@ NO_ID=0
 
 SCRIPT_NAME="checkservices"
 SCRIPT=$(command -v "${SCRIPT_NAME}.sh")
-IDS=$(pgrep -fc "$SCRIPT_NAME")
+NUM_RUNNING_INSTANCES=$(pgrep -fc "$SCRIPT_NAME")
 PROCS=$(pidof -x "$SCRIPT")
 
-if [ "$IDS" -gt "$MIN_ID" ]; then
-   declare -a CHECK_SERVICESARR
-   IFS=' ' read -r -a CHECK_SERVICESARR <<< "$PROCS"
+if [ "$NUM_RUNNING_INSTANCES" -gt "$MIN_ID" ]; then
+   declare -a CHECK_SERVICES_PIDS
+   IFS=' ' read -r -a CHECK_SERVICES_PIDS <<< "$PROCS"
    i=0
-	last_index=$(( ${#CHECK_SERVICESARR[@]} - 1 ))
-	last_value="${CHECK_SERVICESARR[$last_index]}"
-		while [ "${CHECK_SERVICESARR[$i]}" != "$last_value" ]; do
-		echo "${CHECK_SERVICESARR[$i]}"
-		kill "${CHECK_SERVICESARR[$i]}"
+	last_index=$(( ${#CHECK_SERVICES_PIDS[@]} - 1 ))
+	last_value="${CHECK_SERVICES_PIDS[$last_index]}"
+		while [ "${CHECK_SERVICES_PIDS[$i]}" != "$last_value" ]; do
+		echo "${CHECK_SERVICES_PIDS[$i]}"
+		kill "${CHECK_SERVICES_PIDS[$i]}"
 		notify-send -t 10000 --app-name "Check services:" "checkservices instance is already running."
 		i=$((i + 1))
 	done
 
 #elif [ "$CHECK_SERVICES_IDS" -eq $NO_ID ] && [ -z "$CHECK_SERVICES_PROC" ]; then
-elif [ "$IDS" -eq "$NO_ID" ]; then
+elif [ "$NUM_RUNNING_INSTANCES" -eq "$NO_ID" ]; then
 	notify-send --app-name "Check services:" "checkservices is not running. Please check if checkservices process is running"
 	SCRIPT_PATH="$HOME/Documents/bin/${SCRIPT_NAME}.sh"
 	if [ -x "$SCRIPT_PATH" ]; then
