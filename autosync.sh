3d9861b3ff02b8ea80ff29f251fcafa21a19d704
Claive Alvin Marana Polo Adolfo Acedilla <cabpacedilla@gmail.com>
2025-09-11 13:03:19 +0800

    Auto-sync update on 2025-09-11 13:03:19

--- autosync.sh
+++ autosync.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/bash
 # Set user in /etc/sudoers file without providing password with rsync command
 # add line <username> ALL=(ALL) NOPASSWD: /usr/bin/rsync
-#while inotifywait -e modify,create /home/capa/Documents/ /home/capa/bin/
+
 while inotifywait -r -e modify,create /home/claiveapa/Documents/; do
 	BACKUP_TIME=$(date +"%I:%M %p")
-	#notify-send --app-name "Auto-backup:    $BACKUP_TIME" "Folder updated. Syncing folder."
-	#rsync -avHAXS /home/boyet/Documents/claive/ /home/boyet/Documents/backup
-	#rsync -avz /home/boyet/Documents/claive/ /home/boyet/Documents/backup --delete
-	#rsync -avuhP /home/boyet/Documents/claive/ /home/boyet/bin/ /media/boyet/Data/claive
-	#~ sudo rsync -avuhPHAXS /home/claiveapa/Documents /run/media/claiveapa/sda1/claive/Documents/nobara
-	#sudo rsync -avuhPHAXS /home/claiveapa/Documents/ /run/media/claiveapa/Data/claive/Documents/nobara/Documents/
-# 	rsync -avz --protect-args "/home/claiveapa/Documents/" "/run/media/claiveapa/Data/claive/Documents/nobara/"
 	rsync -avz --protect-args "/home/claiveapa/Documents/" "/run/media/claiveapa/Data/claive/Documents/nobara/kde42/"
-	if [ $? = 0 ]; then
-		notify-send --app-name "✅ Auto-backup:    $BACKUP_TIME" "Backup sync was successful."
-	else
-		notify-send --app-name "⚠️ Auto-backup:    $BACKUP_TIME""Backup sync encountered an error."
-	fi
-   #sudo rsync -avHAX ~/Documents/testfiles/ /mnt/backup/ --delete
-   #sudo tar cvf /mnt/backup/tarball$DATE ~/Documents/testfiles/*
+    STATUS=$?
+
+    if [[ $STATUS -eq 0 || $STATUS -eq 24 ]]; then
+        notify-send --app-name "✅ Auto-backup: $BACKUP_TIME" "Backup sync was successful." &
+    else
+        notify-send --app-name "⚠️ Auto-backup: $BACKUP_TIME" "Backup sync encountered an error (code $STATUS)." &
+    fi
 done
